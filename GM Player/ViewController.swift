cb9cb92ee57c5922cb3fcd34761581c10893c086
Frederick Coleman <frc217@gmail.com>
2018-01-30 13:46:34 -0500

    Initial Commit

---
+++ GM Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GM Player
+//
+//  Created by Frederick Coleman on 01/30/18.
+//  Copyright © 2018 GhanaMade Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
